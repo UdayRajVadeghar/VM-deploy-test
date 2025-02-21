ddb99940f79d05a44348e18fb8344872b6825250
Uday <udayraj.vadeghar@gmail.com>
2025-02-21 06:10:50 +0000

    add location to anywhere

--- server.js
+++ server.js
@@ -9,6 +9,6 @@ app.get("/", (req, res) => {
   res.send("Hello, Express!");
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
