cd62d457e1fbd26fc5396fe799a8068e8dd40443
Uday <udayraj.vadeghar@gmail.com>
2025-03-21 11:23:04 +0000

    test

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("ok now check the code npw!");
+  res.send("webhooks testing 1");
 });
 
 app.listen(PORT, "0.0.0.0", () => {
